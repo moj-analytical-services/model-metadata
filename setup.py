0179a9040ee30ff7c34f57fd677a061e804d74f6
Michael Hodge <michael.hodge@digital.justice.gov.uk>
2024-05-16 12:59:13 +0000

    add setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='model-metadata',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'awswrangler',
+        'pandas',
+        'boto3'
+    ],
+    author='Michael Hodge',
+    author_email='michael.hodge1@justice.gov.uk',
+    description='A package for uploading model metadata to Athena',
+    url='https://github.com/moj-analytical-services/model_metadata',
+)
